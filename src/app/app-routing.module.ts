8cf6f58017f22c267b0122e0aca3825f245f3eb5
Silas Prado <silas.prado98@gmail.com>
2023-08-30 17:11:22 -0300

    created nav bar and generated pages components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: AboutComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'fale-conosco',
+    component: ContactUsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
